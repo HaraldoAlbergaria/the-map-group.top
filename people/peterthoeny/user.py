7dda9abfe42e97d9b75cdc328a9173e333434dff
The Map Group <admin@the-map-group.top>
2024-09-04 03:51:53 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/user.py
+++ people/peterthoeny/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Peter Thoeny - Quality HDR Pho',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/98786299@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/peterthoeny/',
-  'markers': 2207,
-  'photos': 3998
+  'markers': 2208,
+  'photos': 3999
 }
