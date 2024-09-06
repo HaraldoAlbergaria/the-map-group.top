c61a56b88ec7e7263a06120e5bc41c39bc39372e
The Map Group <admin@the-map-group.top>
2024-09-06 03:41:15 -0300

    [auto] Updated map for member 'Julian Munilla Rio'

--- people/35690321@N07/countries.py
+++ people/35690321@N07/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
   'NO': ['Norway', 11, 11],
+  'ES': ['Spain', 853, 861],
   'BG': ['Bulgaria', 2, 2],
   'PT': ['Portugal', 125, 125],
-  'ES': ['Spain', 852, 860],
   'IE': ['Ireland', 11, 11]
 }
