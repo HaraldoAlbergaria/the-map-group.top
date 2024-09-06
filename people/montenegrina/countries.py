fc3c6343ed9852290b29f059601a13a7eb96d75e
The Map Group <admin@the-map-group.top>
2024-09-06 03:33:42 -0300

    [auto] Updated map for member 'ForceMajeureMontenegro'

--- people/montenegrina/countries.py
+++ people/montenegrina/countries.py
@@ -1,15 +1,15 @@
 countries_dict = {
+  'GR': ['Greece', 83, 94],
+  'SI': ['Slovenia', 5, 6],
   'HR': ['Croatia', 51, 58],
-  'FR': ['France', 73, 85],
-  'IT': ['Italy', 120, 136],
-  'ES': ['Spain', 109, 128],
+  'MA': ['Morocco', 15, 17],
+  'PT': ['Portugal', 15, 23],
   'CZ': ['Czechia', 49, 58],
   'BA': ['Bosnia and Herzegovina', 18, 20],
-  'MA': ['Morocco', 15, 17],
-  'GR': ['Greece', 83, 94],
-  'AL': ['Albania', 4, 4],
-  'PT': ['Portugal', 15, 23],
+  'IT': ['Italy', 120, 136],
+  'ME': ['Montenegro', 766, 862],
+  'FR': ['France', 73, 85],
   'VA': ['Vatican City', 5, 7],
-  'ME': ['Montenegro', 765, 861],
-  'SI': ['Slovenia', 5, 6]
+  'AL': ['Albania', 4, 4],
+  'ES': ['Spain', 109, 128]
 }
