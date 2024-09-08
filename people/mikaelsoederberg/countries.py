0fb619aa89c2f714d6ed1e04e22bcc2bedbd5132
The Map Group <admin@the-map-group.top>
2024-09-08 02:02:50 -0300

    [auto] Updated map for member 'mikaelsoderberg'

--- people/mikaelsoederberg/countries.py
+++ people/mikaelsoederberg/countries.py
@@ -1,7 +1,7 @@
 countries_dict = {
-  'SE': ['Sweden', 227, 282],
-  'DK': ['Denmark', 4, 4],
+  'DE': ['Germany', 7, 7],
   'CH': ['Switzerland', 3, 4],
-  'DE': ['Germany', 6, 6],
+  'DK': ['Denmark', 4, 4],
+  'SE': ['Sweden', 227, 283],
   'IT': ['Italy', 27, 30]
 }
