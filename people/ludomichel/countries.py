722fe2777ded21cfaee536145c0861c6086892ee
The Map Group <admin@the-map-group.top>
2024-09-08 01:53:19 -0300

    [auto] Updated map for member 'Ludo's World'

--- people/ludomichel/countries.py
+++ people/ludomichel/countries.py
@@ -1,12 +1,12 @@
 countries_dict = {
-  'TH': ['Thailand', 251, 290],
-  'KH': ['Cambodia', 432, 448],
-  'DE': ['Germany', 279, 316],
-  'LU': ['Luxembourg', 38, 38],
-  'BE': ['Belgium', 1649, 1870],
+  'TH': ['Thailand', 627, 722],
+  'FR': ['France', 25, 25],
+  'DO': ['Dominican Republic', 12, 12],
   'US': ['United States of America', 81, 85],
-  'DO': ['Dominican Republic', 3, 3],
-  'NL': ['The Netherlands', 51, 51],
   'VN': ['Vietnam', 385, 427],
-  'FR': ['France', 25, 25]
+  'NL': ['The Netherlands', 51, 51],
+  'DE': ['Germany', 374, 414],
+  'BE': ['Belgium', 2332, 2575],
+  'LU': ['Luxembourg', 86, 86],
+  'KH': ['Cambodia', 432, 448]
 }
--- people/ludomichel/user.py
+++ people/ludomichel/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Lцdо\/іс M',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/124072682@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/ludomichel/',
-  'markers': 3194,
-  'photos': 3553
+  'markers': 4405,
+  'photos': 4845
 }
