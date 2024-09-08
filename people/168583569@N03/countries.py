e3f2fea2db8f283ae8468e9098cc67aad04cde4a
The Map Group <admin@the-map-group.top>
2024-09-08 04:38:45 -0300

    [auto] Updated map for member 'amalthus'

--- people/168583569@N03/countries.py
+++ people/168583569@N03/countries.py
@@ -1,13 +1,18 @@
 countries_dict = {
-  'US': ['United States of America', 1, 1],
-  'IT': ['Italy', 2, 2],
-  'CA': ['Canada', 1, 1],
-  'ES': ['Spain', 1, 1],
-  'AT': ['Austria', 1, 1],
-  'DE': ['Germany', 4, 73],
-  'PT': ['Portugal', 1, 1],
-  'CY': ['Cyprus', 1, 1],
   'BG': ['Bulgaria', 1, 1],
-  'GR': ['Greece', 3, 4],
-  'FR': ['France', 1, 15]
+  'CY': ['Cyprus', 2, 3],
+  'AT': ['Austria', 1, 2],
+  'US': ['United States of America', 15, 113],
+  'HR': ['Croatia', 1, 1],
+  'DE': ['Germany', 13, 228],
+  'CA': ['Canada', 1, 2],
+  'GR': ['Greece', 18, 40],
+  'UG': ['Uganda', 1, 1],
+  'ES': ['Spain', 1, 2],
+  'PT': ['Portugal', 1, 1],
+  'LB': ['Lebanon', 1, 1],
+  'IT': ['Italy', 6, 7],
+  'FR': ['France', 4, 37],
+  'GB': ['United Kingdom', 1, 1],
+  'BR': ['Brazil', 1, 1]
 }
--- people/168583569@N03/user.py
+++ people/168583569@N03/user.py
@@ -2,8 +2,8 @@ user_info = {
   'id': '168583569@N03',
   'alias': '168583569@N03',
   'name': 'amalthus',
-  'avatar': '../../icons/photographer.svg',
+  'avatar': 'https://live.staticflickr.com/5674/buddyicons/168583569@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/168583569@N03/',
-  'markers': 33,
-  'photos': 614
+  'markers': 84,
+  'photos': 1112
 }
