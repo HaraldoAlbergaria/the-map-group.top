f7bac7fb59e667815f77fd7a541f269fed6f3ce1
The Map Group <admin@the-map-group.top>
2024-09-26 03:37:13 -0300

    [auto] Updated map for member 'zimwizdotcom'

--- people/zimwiz/countries.py
+++ people/zimwiz/countries.py
@@ -1,9 +1,9 @@
 countries_dict = {
-  'VN': ['Vietnam', 2, 2],
-  'AQ': ['Antarctica', 1, 1],
+  'AU': ['Australia', 1, 1],
   'GL': ['Greenland', 1, 1],
+  'AQ': ['Antarctica', 1, 1],
   'CA': ['Canada', 4, 4],
+  'VN': ['Vietnam', 2, 2],
   'BM': ['Bermuda', 5, 5],
-  'US': ['United States of America', 621, 877],
-  'AU': ['Australia', 1, 1]
+  'US': ['United States of America', 622, 878]
 }
--- people/zimwiz/user.py
+++ people/zimwiz/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'zimwizdotcom',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/14461370@N03_r.jpg',
   'url': 'https://www.flickr.com/photos/zimwiz/',
-  'markers': 635,
-  'photos': 891
+  'markers': 636,
+  'photos': 892
 }
