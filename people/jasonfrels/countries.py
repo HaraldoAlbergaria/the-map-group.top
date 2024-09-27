2b9d01fb0c4aadc77ed393876b035e662d99230b
The Map Group <admin@the-map-group.top>
2024-09-27 03:21:00 -0300

    [auto] Updated map for member 'Jason Frels'

--- people/jasonfrels/countries.py
+++ people/jasonfrels/countries.py
@@ -1,8 +1,8 @@
 countries_dict = {
-  'SG': ['Singapore', 1, 2],
-  'NO': ['Norway', 15, 16],
-  'CA': ['Canada', 4, 5],
-  'CL': ['Chile', 9, 10],
+  'NO': ['Norway', 17, 18],
   'US': ['United States', 116, 136],
+  'CL': ['Chile', 9, 10],
+  'SG': ['Singapore', 1, 2],
+  'CA': ['Canada', 4, 5],
   'AR': ['Argentina', 1, 1]
 }
