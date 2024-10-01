6d54c832750e247e4840579009fda1c401874183
The Map Group <admin@the-map-group.top>
2024-10-01 03:41:37 -0300

    [auto] Updated map for member 'Jens Koschitzki'

--- people/jenskoschitzki/countries.py
+++ people/jenskoschitzki/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'GB': ['United Kingdom', 147, 150],
-  'FI': ['Finland', 126, 127],
+  'GE': ['Georgia', 155, 176],
+  'LT': ['Lithuania', 125, 136],
+  'IT': ['Italy', 1553, 1934],
+  'DE': ['Germany', 1533, 27891],
+  'EE': ['Estonia', 170, 170],
   'AM': ['Armenia', 161, 172],
   'LV': ['Latvia', 149, 149],
   'AT': ['Austria', 84, 98],
-  'GE': ['Georgia', 155, 176],
-  'EE': ['Estonia', 170, 170],
-  'IT': ['Italy', 1551, 1930],
+  'FI': ['Finland', 126, 127],
   'HR': ['Croatia', 214, 244],
-  'DE': ['Germany', 1533, 27891],
-  'LT': ['Lithuania', 125, 136],
+  'GB': ['United Kingdom', 147, 150],
   'RU': ['Russia', 427, 509]
 }
--- people/jenskoschitzki/user.py
+++ people/jenskoschitzki/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Jens Koschitzki',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/108188718@N06_r.jpg',
   'url': 'https://www.flickr.com/photos/jenskoschitzki/',
-  'markers': 4842,
-  'photos': 31752
+  'markers': 4844,
+  'photos': 31756
 }
