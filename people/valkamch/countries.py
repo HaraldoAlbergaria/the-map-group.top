e2b752014fbfea091e282ac71f43d845cb1c3389
The Map Group <admin@the-map-group.top>
2024-10-04 03:45:07 -0300

    [auto] Updated map for member 'ValKamch'

--- people/valkamch/countries.py
+++ people/valkamch/countries.py
@@ -1,14 +1,14 @@
 countries_dict = {
-  'UA': ['Ukraine', 1279, 1444],
-  'IR': ['Iran', 1, 1],
-  'LK': ['Sri Lanka', 119, 175],
   'NO': ['Norway', 1, 1],
-  'IT': ['Italy', 1370, 1578],
-  'RU': ['Russia', 1455, 1702],
-  'MC': ['Monaco', 5, 12],
-  'KH': ['Cambodia', 316, 378],
-  'FR': ['France', 1, 1],
-  'SG': ['Singapore', 2, 16],
   'UZ': ['Uzbekistan', 169, 189],
-  'MT': ['Malta', 1, 1]
+  'MC': ['Monaco', 5, 12],
+  'IR': ['Iran', 1, 1],
+  'UA': ['Ukraine', 1279, 1444],
+  'RU': ['Russia', 1480, 1728],
+  'IT': ['Italy', 1370, 1578],
+  'LK': ['Sri Lanka', 119, 175],
+  'SG': ['Singapore', 2, 16],
+  'KH': ['Cambodia', 316, 378],
+  'MT': ['Malta', 1, 1],
+  'FR': ['France', 1, 1]
 }
