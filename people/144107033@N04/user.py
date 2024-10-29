266da7f86111c4327012124a009c02b682b995e0
The Map Group <admin@the-map-group.top>
2024-10-29 03:10:58 -0300

    [auto] Updated map for member 'sebastiánaguilar'

--- people/144107033@N04/user.py
+++ people/144107033@N04/user.py
@@ -4,6 +4,6 @@ user_info = {
   'name': 'Sebastián Aguilar',
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/144107033@N04_r.jpg',
   'url': 'https://www.flickr.com/photos/144107033@N04/',
-  'markers': 15369,
-  'photos': 20085
+  'markers': 15383,
+  'photos': 20105
 }
