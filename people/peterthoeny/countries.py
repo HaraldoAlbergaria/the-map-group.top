de62cb9256f19c13db269939a9d88d3382b74903
The Map Group <admin@the-map-group.top>
2024-10-29 03:51:10 -0300

    [auto] Updated map for member 'PeterThoeny'

--- people/peterthoeny/countries.py
+++ people/peterthoeny/countries.py
@@ -1,17 +1,17 @@
 countries_dict = {
-  'UA': ['Ukraine', 1, 1],
-  'CH': ['Switzerland', 226, 386],
-  'IS': ['Iceland', 66, 97],
-  'GB': ['United Kingdom', 17, 34],
-  'CA': ['Canada', 6, 6],
-  'JP': ['Japan', 121, 144],
   'EE': ['Estonia', 1, 1],
   'IT': ['Italy', 34, 48],
-  'LI': ['Liechtenstein', 4, 5],
-  'US': ['United States of America', 1675, 3217],
-  'AT': ['Austria', 6, 9],
-  'GL': ['Greenland', 5, 5],
+  'CH': ['Switzerland', 226, 386],
+  'GB': ['United Kingdom', 17, 34],
+  'UA': ['Ukraine', 1, 1],
   'CN': ['South China Sea Islands (disputed)', 71, 95],
+  'US': ['United States of America', 1675, 3218],
+  'CA': ['Canada', 6, 6],
+  'NO': ['Norway', 1, 1],
   'DE': ['Germany', 2, 3],
-  'NO': ['Norway', 1, 1]
+  'IS': ['Iceland', 66, 97],
+  'AT': ['Austria', 6, 9],
+  'JP': ['Japan', 121, 144],
+  'GL': ['Greenland', 5, 5],
+  'LI': ['Liechtenstein', 4, 5]
 }
--- people/peterthoeny/user.py
+++ people/peterthoeny/user.py
@@ -5,5 +5,5 @@ user_info = {
   'avatar': 'https://live.staticflickr.com/5674/buddyicons/98786299@N00_r.jpg',
   'url': 'https://www.flickr.com/photos/peterthoeny/',
   'markers': 2237,
-  'photos': 4053
+  'photos': 4054
 }
